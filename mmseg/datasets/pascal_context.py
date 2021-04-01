94ce7d79e1fc1f0f44c389c19c71f7cf9ede236d
sixiaozheng <sxzheng18@gmail.com>
2021-04-01 13:51:45 +0800

    Clean up unused code of pascal_context.py

--- mmseg/datasets/pascal_context.py
+++ mmseg/datasets/pascal_context.py
@@ -1,10 +1,4 @@
 import os.path as osp
-import tempfile
-
-import mmcv
-import numpy as np
-from mmcv.utils import print_log
-from PIL import Image
 
 from .builder import DATASETS
 from .custom import CustomDataset
@@ -18,7 +12,7 @@ class PascalContextDataset(CustomDataset):
     which is included in 60 categories. ``reduce_zero_label`` is fixed to
     False. The ``img_suffix`` is fixed to '.jpg' and ``seg_map_suffix`` is
     fixed to '.png'.
-
+    
     Args:
         split (str): Split txt file for PascalContext.
     """
@@ -58,86 +52,3 @@ class PascalContextDataset(CustomDataset):
             reduce_zero_label=False,
             **kwargs)
         assert osp.exists(self.img_dir) and self.split is not None
-
-    @staticmethod
-    def _convert_to_label_id(result):
-        """Convert trainId to id for cityscapes."""
-        import cityscapesscripts.helpers.labels as CSLabels
-        result_copy = result.copy()
-        for trainId, label in CSLabels.trainId2label.items():
-            result_copy[result == trainId] = label.id
-
-        return result_copy
-
-    def results2img(self, results, imgfile_prefix, to_label_id):
-        """Write the segmentation results to images.
-
-        Args:
-            results (list[list | tuple | ndarray]): Testing results of the
-                dataset.
-            imgfile_prefix (str): The filename prefix of the png files.
-                If the prefix is "somepath/xxx",
-                the png files will be named "somepath/xxx.png".
-            to_label_id (bool): whether convert output to label_id for
-                submission
-
-        Returns:
-            list[str: str]: result txt files which contains corresponding
-            semantic segmentation images.
-        """
-        mmcv.mkdir_or_exist(imgfile_prefix)
-        result_files = []
-        prog_bar = mmcv.ProgressBar(len(self))
-        for idx in range(len(self)):
-            result = results[idx]
-            if to_label_id:
-                result = self._convert_to_label_id(result)
-            filename = self.img_infos[idx]['filename']
-            basename = osp.splitext(osp.basename(filename))[0]
-
-            png_filename = osp.join(imgfile_prefix, f'{basename}.png')
-
-            output = Image.fromarray(result.astype(np.uint8)).convert('P')
-            import cityscapesscripts.helpers.labels as CSLabels
-            palette = np.zeros((len(CSLabels.id2label), 3), dtype=np.uint8)
-            for label_id, label in CSLabels.id2label.items():
-                palette[label_id] = label.color
-
-            output.putpalette(palette)
-            output.save(png_filename)
-            result_files.append(png_filename)
-            prog_bar.update()
-
-        return result_files
-
-    def format_results(self, results, imgfile_prefix=None, to_label_id=True):
-        """Format the results into dir (standard format for Cityscapes
-        evaluation).
-
-        Args:
-            results (list): Testing results of the dataset.
-            imgfile_prefix (str | None): The prefix of images files. It
-                includes the file path and the prefix of filename, e.g.,
-                "a/b/prefix". If not specified, a temp file will be created.
-                Default: None.
-            to_label_id (bool): whether convert output to label_id for
-                submission. Default: False
-
-        Returns:
-            tuple: (result_files, tmp_dir), result_files is a list containing
-                the image paths, tmp_dir is the temporal directory created
-                for saving json/png files when img_prefix is not specified.
-        """
-
-        assert isinstance(results, list), 'results must be a list'
-        assert len(results) == len(self), (
-            'The length of results is not equal to the dataset len: '
-            f'{len(results)} != {len(self)}')
-
-        if imgfile_prefix is None:
-            tmp_dir = tempfile.TemporaryDirectory()
-            imgfile_prefix = tmp_dir.name
-        else:
-            tmp_dir = None
-        result_files = self.results2img(results, imgfile_prefix, to_label_id)
-        return result_files, tmp_dir
